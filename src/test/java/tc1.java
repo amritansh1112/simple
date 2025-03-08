35b83661168c7726b83fceb80fb621858543e2c2
unknown <b63user12@rpsconsulting.in>
2025-03-08 01:25:49 -0800

    inital commit

---
+++ src/test/java/tc1.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+ 
+public class tc1 {
+ 
+  @Test
+  public void test() {
+  	System.out.println("Hello World");
+  }
+ 
+}
+
+
