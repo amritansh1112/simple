e085090794356ace82119b3992e76a98b6483c51
unknown <b63user12@rpsconsulting.in>
2025-03-08 02:26:35 -0800

    Second commit

--- src/test/java/tc2.java
+++ src/test/java/tc2.java
@@ -1,12 +1,10 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class tc2 {
 
 	@Test
 	public void test() {
-		System.out.println("test case 2 running ")
+		System.out.println("test case 2 running ");
 		}
 
 }
